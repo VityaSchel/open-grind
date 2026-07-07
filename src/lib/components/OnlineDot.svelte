900a6e0c7281a8301c0255db201f823bb9de6e3b
KinkyMonk <greekkoala@yahoo.com>
2026-07-07 20:37:18 +0200

    Changed visiting icon and cleaned up

--- src/lib/components/OnlineDot.svelte
+++ src/lib/components/OnlineDot.svelte
@@ -14,31 +14,44 @@
 	$effect(() => subscribeNow());
 
 	const online = $derived(onlineUntil != null && onlineUntil > getNow());
+    const title = $derived.by(() => {
+        if (online && isVisiting) {
+            return 'Online now. Visiting';
+        }
+        if (online) {
+            return 'Online now';
+        }
+        if (isVisiting) {
+            return 'Visiting';
+        }
+    });
 </script>
 
 {#if isVisiting || online}
-	<span class={[online ? "text-green-500" : "text-gray-400"]}>
-		{#if isVisiting}
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				height="1em"
-				width="1em"
-				fill="currentColor"
-				viewBox="0 -960 960 960"
-			>
-				<path
-					d="m397-115-99-184-184-99 71-70 145 25 102-102-317-135 84-86 385 68 124-124q23-23 57-23t57 23q23 23 23 56.5T822-709L697-584l68 384-85 85-136-317-102 102 26 144-71 71Z"
-				/>
-			</svg>
-		{:else if online}
-			<span
-				class={[
-					"inline-block size-2 shrink-0 rounded-full bg-green-500",
-					className,
-				]}
-				aria-label="Online now"
-				title="Online now"
-			></span>
-		{/if}
-	</span>
+    <span
+        class={[
+            "inline-block size-2 shrink-0",
+            !isVisiting && "rounded-full bg-green-500",
+            online ? "text-green-500" : "text-gray-400",
+            className,
+            ]}
+            aria-label={title}
+	        title={title}
+    >
+        {#if isVisiting}
+            <!-- Author: michaelampr. MIT License-->
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+                viewBox="-1 -1 24 24"
+                width="100%"
+                height="100%"
+                preserveAspectRatio="xMinYMin"
+            >
+                <path
+                    d='M12.685 13.285l-3.44 3.06.528 2.423-2.488 2.488-2.507-3.921-3.921-2.507 2.488-2.488L5.9 13l2.927-3.573-6.171-4.114 2.828-2.829L13.2 5.057l3.793-3.793c1.171-1.172 2.985-1.258 4.05-.193s.978 2.878-.193 4.05l-3.793 3.793 2.571 7.713-2.828 2.829-4.114-6.171z'
+                />
+            </svg>
+        {/if}
+    </span> 
 {/if}
