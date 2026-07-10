966b05edc079deb53d241f9e6f46cda814f58d77
Viktor Shchelochkov <hi@hloth.dev>
2026-07-10 09:33:38 +0100

    Replace svg with Phosphor Icons component

--- src/lib/components/OnlineDot.svelte
+++ src/lib/components/OnlineDot.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import { AirplaneTiltIcon } from "phosphor-svelte";
+
 	import { getNow, subscribeNow } from "$lib/now.svelte";
 
 	let {
@@ -28,30 +30,22 @@
 </script>
 
 {#if isVisiting || online}
-    <span
+    <div
         class={[
-            "inline-block size-2 shrink-0",
-            !isVisiting && "rounded-full bg-green-500",
-            online ? "text-green-500" : "text-gray-400",
-            className,
-            ]}
-            aria-label={title}
-	        title={title}
+            "flex shrink-0",
+			{
+				"text-green-500": online,
+				"text-gray-400": !online
+			},
+			className,
+		]}
+		aria-label={title}
+		title={title}
     >
         {#if isVisiting}
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="currentColor"
-                viewBox="0 0 256 256"
-                width="100%"
-                height="100%"
-                class="scale-125"
-            >
-                <!-- Phosphor. MIT License-->
-                <path
-                    d="M215.52,197.26a8,8,0,0,1-1.86,8.39l-24,24A8,8,0,0,1,184,232a7.09,7.09,0,0,1-.79,0,8,8,0,0,1-5.87-3.52l-44.07-66.12L112,183.59V208a8,8,0,0,1-2.34,5.65s-14,14.06-15.88,15.88A7.91,7.91,0,0,1,91,231.41a8,8,0,0,1-10.41-4.35l-.06-.15-14.7-36.76L29,175.42a8,8,0,0,1-2.69-13.08l16-16A8,8,0,0,1,48,144H72.4l21.27-21.27L27.56,78.65a8,8,0,0,1-1.22-12.32l24-24a8,8,0,0,1,8.39-1.86l85.94,31.25L176.2,40.19a28,28,0,0,1,39.6,39.6l-31.53,31.53Z"
-                />
-            </svg>
+            <AirplaneTiltIcon weight="fill" class="size-3" />
+		{:else}
+			<span class="rounded-full bg-green-500 size-2"></span>
         {/if}
-    </span> 
+    </div> 
 {/if}
