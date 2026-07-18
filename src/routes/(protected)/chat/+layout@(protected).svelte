8190a9050f668a91c5e50424f9cb7e9a43762708
Viktor Shchelochkov <hi@hloth.dev>
2026-07-18 16:30:52 +0100

    Fix eslint warning introduced by bc5a82f0427476bf8ef0c8539a0303dd3d5eb99e

--- src/routes/(protected)/chat/+layout@(protected).svelte
+++ src/routes/(protected)/chat/+layout@(protected).svelte
@@ -72,7 +72,7 @@
 				<div class="h-full flex-1 self-stretch p-4 ps-1 pb-nav-clear">
 					<Card.Root
 						class={[
-							"relative h-full gap-0 rounded-2xl p-0 dark:ring-neutral-800 rounded-[27px]",
+							"relative h-full gap-0 rounded-[27px] p-0 dark:ring-neutral-800",
 							{
 								"bg-card/20 ring-0": !isChatSelected,
 							},
