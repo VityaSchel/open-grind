7aa1d8f8f09a17fd471c49b441ba0885b8c6ea30
Viktor Shchelochkov <hi@hloth.dev>
2026-09-10 18:08:46 +0200

    Add FAQ/Guides links to opengrind.org header

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -64,6 +64,8 @@ export default defineConfig({
 
 		nav: [
 			{ text: "Home", link: "/" },
+			{ text: "FAQ", link: "https://opengrind.org/guides/faq" },
+			{ text: "Guides", link: "https://opengrind.org/guides/features/unlimited-profiles" },
 			{ text: "Grindr API", link: "/grindr-api" },
 		],
 
